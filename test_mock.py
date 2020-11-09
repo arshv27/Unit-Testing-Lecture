e9582b85fd443770aff02dd6a8d880c932aabdcd
Arsh Verma <arshverma@iiitd.ac.in>
2020-11-10 03:42:44 +0530

    Added code for mock objects

--- test_mock.py
+++ test_mock.py
@@ -1,37 +1,66 @@
+# import unittest
+# from mock_class import MyClass
+
+# class MockDataSource(object):
+# 	def __init__(self):
+# 		self.readFrom = False
+# 		self.closed = False
+
+# 	def read(self):
+# 		self.readFrom = True
+# 		return 'some data'
+
+# 	def close(self):
+# 		self.closed = True
+
+
+# class TestMyClass(unittest.TestCase):
+
+# 	def testConstructor(self):
+# 		"Test the default state"
+# 		myclass = MyClass()
+
+# 		self.assertEqual(myclass.data, None)
+
+# 	def testReadData(self):
+# 		myclass = MyClass()
+
+# 		source = MockDataSource()
+
+# 		myclass.readData(source)
+
+# 		self.assertEqual(myclass.data, 'some data')
+# 		self.assertTrue(source.readFrom)
+# 		self.assertTrue(source.closed)
+
+# if (__name__ == '__main__'):
+# 	unittest.main()
+
+
 import unittest
-from mock import MyClass
+from mock import Mock
 
-class MockDataSource(object):
-    def __init__(self):
-        self.readFrom = False
-        self.closed = False
-
-    def read(self):
-        self.readFrom = True
-        return 'some data'
-
-    def close(self):
-        self.closed = True
-
+from mock_class import MyClass
 
 class TestMyClass(unittest.TestCase):
 
-    def testConstructor(self):
-        "Test the default state"
-        myclass = MyClass()
+	def testConstructor(self):
+		"Test the default state"
+		myclass = MyClass()
 
-        self.assertEqual(myclass.data, None)
+		self.assertEqual(myclass.data, None)
 
-    def testReadData(self):
-        myclass = MyClass()
+	def testReadData(self):
+		myclass = MyClass()
 
-        source = MockDataSource()
+		source = Mock()
+		source.read.return_value = 'some data'
 
-        myclass.readData(source)
+		myclass.readData(source)
 
-        self.assertEqual(myclass.data, 'some data')
-        self.assertTrue(source.readFrom)
-        self.assertTrue(source.closed)
+		self.assertEqual(myclass.data, 'some data')
+		self.assertTrue(source.read.called)
+		self.assertTrue(source.close.called)
 
 if (__name__ == '__main__'):
 	unittest.main()
